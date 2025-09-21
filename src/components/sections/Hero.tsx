b6150eacc8785d60ac21b2dfadac4630ad75df96
sarvesh talekar <sarveshtalekar123@gmail.com>
2025-09-21 13:24:43 +0530

    changes in profile pic

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import { Button } from '../../components/ui/button';
 import { Download, Mail, ArrowDown, User } from 'lucide-react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import profileImage from '../../../public/assets/profile.jpg';
+import profileImage from '/assets/profile.png';
 
 const Hero = () => {
   // Configuration constants
